861ac7c73c1459d3c20ae755cce8af214787d465
Morgan Corder <morgancorder1056@gmail.com>
2025-03-12 20:41:22 -0400

    Time: 630 ms (13.91%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM
+    Cinema
+WHERE
+    description NOT LIKE '%boring%' AND
+    (id % 2 <> 0)
+ORDER BY 
+    rating DESC;
